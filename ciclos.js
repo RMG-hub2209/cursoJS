a175e2af240bb348a34ef334d177566afe0c13ef
surftware-RMG <rafael.madrigal@ciencias.unam.mx>
2021-10-27 13:44:57 -0500

    ciclos

---
+++ ciclos.js
@@ -0,0 +1,31 @@
+//ciclo while
+//el ciclo while evalua si una condición es verdadera o falsa,
+//mientras sea verdadera, el ciclo se va a repetir según la expresión y el contador,
+//el contador es la variable la cual será evaluada
+/*
+var contador = 0;
+
+while(contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del programa");*/
+
+//ciclo do while
+//el ciclo do while ejecuta un bloque de código que sea verdadero,
+//mientras el código sea verdadero se va a repetir el bloque,
+//cuando sea falso, el código al menos se ejecutó una vez
+ /*do{
+     console.log(contador);
+     contador++;
+ }while(
+     contador < 3
+ );
+ console.log("Fin del ciclo do while");
+ */
+
+ for (let contador = 0; contador < 3; contador++){
+     console.log(contador);
+ }
+ console.log("Fin de ciclo for");
+
