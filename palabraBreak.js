6cedeb0e71b56f065523edd78ffdb86c5650933b
surftware-RMG <rafael.madrigal@ciencias.unam.mx>
2021-11-10 15:13:11 -0600

    etiquetas,break y continue

---
+++ palabraBreak.js
@@ -0,0 +1,16 @@
+// for(contador = 0; contador < 10; contador++){
+//     if (contador % 2 == 0){
+//         console.log(contador);
+//         break;
+//     }
+// }
+contador = 0;
+while(contador <= 10){
+    console.log(contador);
+    if()
+    contador++;
+}
+    
+
+console.log("Fin del programa");
+
