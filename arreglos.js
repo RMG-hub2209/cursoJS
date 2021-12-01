5fbafbfccf274b27e01c0b435e0a279817f3a099
RMG-hub2209 <rafael.madrigal@ciencias.unam.mx>
2021-12-01 12:49:09 -0600

    agregar elementos, modificar, preguntar si es un array

--- arreglos.js
+++ arreglos.js
@@ -12,3 +12,25 @@ console.log(autos[0]);
 for (let i = 0; i < autos.length; i++){
     console.log(i + ' : ' + autos [i]);
 }
+
+/*así se pueden modificar los elementos de un arreglo*/
+autos[1] = 'MercedesBenz';
+console.log(autos);
+
+/*con push, agregamos más elementos al arreglo*/
+autos.push('McLaren');
+console.log(autos);
+
+// con autos.length podemos agregar tambien valores al sigueinte índice en el arreglo
+autos [autos.length] = 'RUF';
+
+/*tambien podemos agregar elementos específicamente a un índice, pero esto dejaría espacios en blanco
+entre los índices*/
+autos[6] = 'Porsche';
+console.log(autos);
+
+//para preguntar si es un arreglo podemos usar Array.isArray()
+console.log(Array.isArray(autos));
+
+// tambien podemos usar instanceof
+console.log(autos instanceof Array);
